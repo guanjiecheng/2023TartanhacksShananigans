79fc42f134e8cda4dad13bad1d4a34904424007c
Shanting <shantingh2021@student.cis.edu.hk>
2023-02-04 08:30:04 -0500

    slay

--- templates/src/components/header.js
+++ templates/src/components/header.js
@@ -6,6 +6,11 @@ export default function header(){
     return(
         <div className = "header">
             <img id = "logo" src={require("../images/logo.png")}></img>
+            <div className="heading">
+                <h1>BRINGING</h1>
+                <h1 class="beauty">beauty</h1>
+                <h1>TO THE BIDDING</h1>
+            </div>
         </div>
     )
 }
