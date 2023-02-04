dec29d7ed136cb03d632527d4bab8f8f13b1a5a6
guanjiecheng <gcheng014@gmail.com>
2023-02-04 04:06:01 -0500

    media

---
+++ templates/src/components/estimator.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function estimator(){
+    const mediums = ['Oil on Canvas','Acrylic', 'Chalk', 'Watercolor', 'Mixed Media', 'Etching', 'Graphite', 'Digital','Photography', 'Others']
+    return(
+        <div className = "estimator">
+            <h3>SEE FUTURE TRENDS... BEFORE THEY OCCUR</h3>
+            <input type = "search" id = "artist-name">SEARCH BY ARTIST NAME</input>
+            <h5>MEDIUM</h5>
+            <div>
+
+            </div>
+        </div>
+    )
+}
---
+++ templates/src/components/mediums.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function medium(props){
+    return(
+        <button className = "mediums">
+            {props.name}
+        </button>
+    )
+}
