7eead46ef4b84e38a61c9a6e4140f08f574f39b1
Bchenjamin <bzchen@andrew.cmu.edu>
2023-02-04 02:26:48 -0500

    BACK AND FRONT HAS BEEN LINKED

--- templates/src/App.js
+++ templates/src/App.js
@@ -1,24 +1,40 @@
+import { useState, useEffect } from 'react'
+// import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   // new line start
+  const [profileData, setProfileData] = useState(null)
+
+  useEffect(() => {
+    // setProfileData(({
+    //   profile_name: "ben",
+    //   about_me: "is student"}))}
+    fetch("/profile").then((res) =>
+            res.json().then((data) => {
+                // Setting a data from api
+                setProfileData({
+                    name: data.name,
+                    about: data.about,
+                });
+            })
+    );
+  })
+
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      "this should render"
+      <header className="App-header">
+        {/* new line start*/}
+        <p>To get your profile details: </p>
+        {profileData && <div>
+              <p>Profile name: {profileData.name}</p>
+              <p>About me: {profileData.about}</p>
+            </div>
+        }
+         {/* end of new line */}
+      </header>
     </div>
   );
 }
